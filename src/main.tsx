98b87f9625dd380ee595b14942ca1cdfa40f2023
Saeedinum <mo7amed0ptr@gmail.com>
2024-09-28 03:49:08 +0300

    chore: add @vercel/speed-insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
-import { createRoot } from "react-dom/client";
-import Router from "./router/Router";
-import { store } from "./store/store";
-import { Provider } from "react-redux";
-import "./style/index.css";
+import { createRoot } from "react-dom/client"
+
+import Router from "./router/Router"
+
+import { Provider } from "react-redux"
+import { store } from "./store/store"
+
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
+import "./style/index.css"
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <Router />
+    <SpeedInsights />
   </Provider>,
-);
+)
