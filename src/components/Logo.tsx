3743f7f3a8e523737b3ed761408c41b2a115a417
Saeedinum <mo7amed0ptr@gmail.com>
2024-10-04 19:37:41 +0300

    fix: new logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom"
-import earthLogo from "/src/assets/global/earth.svg"
+import earthLogo from "/public/earth.jpg"
 
 const Logo: React.FC = () => {
   return (
     <Link to={"/"} className="flex items-center gap-2">
-      <img src={earthLogo} alt="logo " className="" />
+      <img src={earthLogo} alt="logo " className="size-[40px]" />
       <h1 className="bg-gradient-to-b from-[#528FCC] to-[#86A41E] bg-clip-text text-2xl font-bold text-transparent">
        EcoChain
       </h1>
