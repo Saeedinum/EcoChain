beee847379615a81d61d5ba6c2bc57c76abb232e
Saeedinum <mo7amed0ptr@gmail.com>
2024-10-05 13:02:10 +0300

    feat: update scenarios images and simplify score handling logic

--- src/pages/senarios/Scenarios.tsx
+++ src/pages/senarios/Scenarios.tsx
@@ -1,28 +1,38 @@
 import { useState, useEffect } from "react"
 
-import image1before from "@/assets/senarios/image1before.png"
-import image1after from "@/assets/senarios/image1after.png"
+import wildfireOne from "@/assets/senarios/wildfire/1.png"
+import wildfireTwo from "@/assets/senarios/wildfire/2.png"
+import wildfireThree from "@/assets/senarios/wildfire/3.png"
+import wildfireFour from "@/assets/senarios/wildfire/4.png"
+import wildfireFive from "@/assets/senarios/wildfire/5.png"
+
+import iceMeltOne from "@/assets/senarios/iceMelt/1.png"
+import iceMeltTwo from "@/assets/senarios/iceMelt/2.png"
+import iceMeltThree from "@/assets/senarios/iceMelt/3.png"
+import iceMeltFour from "@/assets/senarios/iceMelt/4.png"
+import iceMeltFive from "@/assets/senarios/iceMelt/5.png"
 
 import image2before from "@/assets/senarios/image2before.png"
 import image2after from "@/assets/senarios/image2after.png"
 
-import image3before from "@/assets/senarios/image3before.png"
-import image3after from "@/assets/senarios/image3after.png"
-
 import { Progress } from "@/components/ui/progress"
 
 const Scenarios = () => {
-  const [effect, setEffect] = useState(false)
   const [score, setScore] = useState(0)
   const [isDesktop, setIsDesktop] = useState(true)
-
-  const [imageSources, setImageSources] = useState([image1before, image2before, image3before])
+  const [imageSources, setImageSources] = useState([wildfireOne, image2before, iceMeltOne])
 
   useEffect(() => {
     if (score === 100) {
-      setImageSources([image1after, image2after, image3after])
-    } else if (score === 0) {
-      setImageSources([image1before, image2before, image3before])
+      setImageSources([wildfireFive, image2after, iceMeltFive])
+    } else if (score >= 80) {
+      setImageSources([wildfireFour, image2before, iceMeltFour])
+    } else if (score >= 60) {
+      setImageSources([wildfireThree, image2before, iceMeltThree])
+    } else if (score >= 40) {
+      setImageSources([wildfireTwo, image2before, iceMeltTwo])
+    } else {
+      setImageSources([wildfireOne, image2before, iceMeltOne])
     }
   }, [score])
 
@@ -42,39 +52,11 @@ const Scenarios = () => {
   }, [])
 
   const handleIncrement = () => {
-    if (score < 100 && !effect) {
-      setEffect(true)
-
-      const interval = setInterval(() => {
-        setScore(prevScore => {
-          if (prevScore < 100) {
-            return prevScore + 1
-          } else {
-            clearInterval(interval)
-            setEffect(false)
-            return prevScore
-          }
-        })
-      }, 10)
-    }
+    setScore(prevScore => Math.min(prevScore + 10, 100))
   }
 
   const handleDecrement = () => {
-    if (score > 0 && !effect) {
-      setEffect(true)
-
-      const interval = setInterval(() => {
-        setScore(prevScore => {
-          if (prevScore > 0) {
-            return prevScore - 1
-          } else {
-            clearInterval(interval)
-            setEffect(false)
-            return prevScore
-          }
-        })
-      }, 10)
-    }
+    setScore(prevScore => Math.max(prevScore - 10, 0))
   }
 
   const getProgressColor = (score: number) => {
@@ -97,19 +79,20 @@ const Scenarios = () => {
 
   return (
     <main className="flex flex-col">
+      <h1 className="mb-5 text-center text-2xl font-bold text-blue-700">How climate changes affect our planet</h1>
       <section className="flex items-center">
-        <button onClick={handleDecrement} disabled={effect} className="mx-10 place-self-end rounded-[10px] bg-blue-600 px-3 py-2 font-bold text-white">
+        <button onClick={handleDecrement} className="mx-10 place-self-end rounded-[10px] bg-blue-600 px-3 py-2 font-bold text-white">
           Reverse Effect
         </button>
         <Progress value={score} className={`${getProgressColor(score)} mx-auto max-w-[700px]`} />
-        <button onClick={handleIncrement} disabled={effect} className="mx-10 place-self-end rounded-[10px] bg-red-600 px-3 py-2 font-bold text-white">
+        <button onClick={handleIncrement} className="mx-10 place-self-end rounded-[10px] bg-red-600 px-3 py-2 font-bold text-white">
           Add Effect
         </button>
       </section>
       <section className="my-20 flex justify-center gap-10 *:w-[300px]">
-        <img src={imageSources[0]} alt="Image 1" />
+        <img src={imageSources[0]} alt="Wildfire progression" />
         <img src={imageSources[1]} alt="Image 2" />
-        <img src={imageSources[2]} alt="Image 3" />
+        <img src={imageSources[2]} alt="Ice Melt progression" />
       </section>
     </main>
   )
