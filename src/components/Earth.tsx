b4db03523035c7867e39bfa5e0dc8ae4f8ab0f0c
Saeedinum <mo7amed0ptr@gmail.com>
2024-10-11 16:18:47 +0300

    feat: add Earth 3D model and integrate with HeroSection using react-three-fiber

---
+++ src/components/Earth.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+import { useGLTF } from "@react-three/drei"
+import { Mesh } from "three"
+import { GroupProps } from "@react-three/fiber"
+
+export default function Model(props: GroupProps) {
+  const { nodes, materials } = useGLTF("/public/earth.gltf")
+
+  const [scale, setScale] = useState(3)
+
+  useEffect(() => {
+    const handleResize = () => {
+      let newScale
+      if (window.innerWidth < 640) {
+        newScale = 2
+      } else if (window.innerWidth < 768) {
+        newScale = 2.4
+      } else if (window.innerWidth < 1024) {
+        newScale = 2.7
+      } else {
+        newScale = 3
+      }
+      setScale(newScale)
+    }
+
+    window.addEventListener("resize", handleResize)
+    handleResize()
+
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+  return (
+    <group {...props} dispose={null}>
+      <mesh geometry={(nodes.Object_4 as Mesh).geometry} material={materials["Scene_-_Root"]} scale={scale} />
+    </group>
+  )
+}
+
+useGLTF.preload("/public/earth.gltf")
--- src/pages/home/components/HeroSection.tsx
+++ src/pages/home/components/HeroSection.tsx
@@ -1,25 +1,14 @@
-import { useEffect, useState } from "react"
-
 import leftBackground from "@/assets/home/leftbackground.png"
 import rightBackground from "@/assets/home/rightbackground.png"
-import animation1Image from "@/assets/home/animation1.png"
-import animation2Image from "@/assets/home/animation2.png"
-import animation3Image from "@/assets/home/animation3.png"
-import animation4Image from "@/assets/home/animation4.png"
+
+import { Canvas } from "@react-three/fiber"
+import { Environment, OrbitControls } from "@react-three/drei"
+import { Suspense } from "react"
+import Earth from "@/components/Earth"
 
 const HeroSection = () => {
-  const images = [animation1Image, animation2Image, animation3Image, animation4Image]
-
-  const [currentImageIndex, setCurrentImageIndex] = useState(0)
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex(prevIndex => (prevIndex + 1) % images.length)
-    }, 2000)
-    return () => clearInterval(interval)
-  }, [images.length])
-
   return (
-    <section className="relative flex flex-col items-center overflow-hidden pb-20 max-lg:gap-40 lg:flex-row lg:justify-between lg:px-[5rem] lg:pb-[15rem]">
+    <section className="relative flex flex-col items-center gap-10 overflow-hidden pb-20 lg:h-screen lg:flex-row lg:justify-between lg:px-[5rem] lg:pb-[15rem]">
       <div className="flex flex-col items-center gap-10">
         <h2 className="flex flex-col items-center gap-2 text-nowrap text-center text-[20px] font-extrabold text-[#0B539B] lg:text-[45px]">
           <span>WHAT</span>
@@ -27,18 +16,15 @@ const HeroSection = () => {
           <span>DO ?!</span>
         </h2>
       </div>
-      <img src={animation1Image} alt="" className="place-self-center max-lg:w-[325px] lg:hidden" />
-      <div className="relative hidden h-[500px] w-[520px] lg:block">
-        {images.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            alt=""
-            className={`absolute w-full transform transition-all duration-1000 ${index === currentImageIndex ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"}`}
-            style={{ transitionTimingFunction: "ease-in-out" }}
-          />
-        ))}
-      </div>
+
+      <Canvas className="h-[600px] w-[700px]">
+        {/* <ambientLight /> */}
+        <OrbitControls />
+        <Suspense fallback={null}>
+          <Earth />
+        </Suspense>
+        <Environment preset="sunset" />
+      </Canvas>
       <img src={leftBackground} alt="" className="absolute left-0 top-0 -z-10 max-lg:w-[452px]" />
       <img src={rightBackground} alt="" className="absolute right-0 -z-10 max-lg:top-[400px] max-lg:w-[384px] lg:top-0" />
     </section>
